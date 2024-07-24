e37be639b5d6cf794fbee99b4862be19ebc1e95b
LeetDimas1337 <142295284+LeetDimas1337@users.noreply.github.com>
2024-07-24 09:50:42 +0300

    delete all files

--- src/widgets/comments/ui/divider/Divider.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import classes from './divider.module.scss'
-const Divider = (): JSX.Element => {
-    return (
-        <hr className={classes.divider}/>
-    );
-};
-
-export default Divider;
